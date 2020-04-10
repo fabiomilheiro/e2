376ae40f77adf778fb412bc94fa3f243ff65004f
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-04-10 08:48:06 +0100

    Drafted NavBar loading styles. Removed App.css styles.

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
+
+class NavBar extends Component {
+  state = {};
+  render() {
+    const activeItem = "features";
+
+    return (
+      <Menu stackable>
+        <Menu.Item>
+          <img src="https://react.semantic-ui.com/logo.png" />
+        </Menu.Item>
+
+        <Menu.Item
+          name="features"
+          active={activeItem === "features"}
+          onClick={this.handleItemClick}
+        >
+          Features
+        </Menu.Item>
+
+        <Menu.Item
+          name="testimonials"
+          active={activeItem === "testimonials"}
+          onClick={this.handleItemClick}
+        >
+          Testimonials
+        </Menu.Item>
+
+        <Menu.Item
+          name="sign-in"
+          active={activeItem === "sign-in"}
+          onClick={this.handleItemClick}
+        >
+          Sign-in
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+export default NavBar;
