f3c3f52b9ddff8e546d2dccbd4402b642cb36b44
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-04-10 09:07:12 +0100

    Added styles for page not found.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
+import "../styles/pages/NotFound.scss";
 
 const NotFound = () => {
   return (
-    <Grid>
+    <Grid centered className="not-found">
       <Grid.Row>
         <Grid.Column>
           <h1>Not found</h1>
+          <p>The page was not found.</p>
         </Grid.Column>
       </Grid.Row>
     </Grid>
