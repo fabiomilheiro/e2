be8387acb72aecf400af2f1699e62b181eccbe65
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-04-13 15:13:53 +0100

    Drafted default theme.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
-import "semantic-ui-css/semantic.min.css";
 import { Route, Redirect, Switch } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Persons from "./pages/Persons";
@@ -9,6 +8,10 @@ import Groups from "./pages/Groups";
 import { Container } from "semantic-ui-react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import "./styles/themes/default/_index.scss";
+
+// const theme = "default";
+// require(`./styles/themes/${theme}/_index.scss`);
 
 function App() {
   return (
